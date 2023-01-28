057dfe87bc556c56cf4334039035c93dd298d58a
Kazurin Nanako <71819243+Kazurin-775@users.noreply.github.com>
2023-01-28 20:18:43 +0800

    Introduce `dio_http_cache`

--- lib/global.dart
+++ lib/global.dart
@@ -1,8 +1,11 @@
 import 'package:bililive_api_fl/bililive_api_fl.dart';
 import 'package:dio/dio.dart';
+import 'package:dio_http_cache/dio_http_cache.dart';
 import 'package:dio_throttler/dio_throttler.dart';
 import 'package:logger/logger.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'sqflite_shim.dart';
 
 class Global {
   static late final Global i;
@@ -12,11 +15,15 @@ class Global {
   late final SharedPreferences prefs;
 
   static Future<void> init() async {
+    // Initialize sqflite for use by dio_http_cache on Windows
+    sqfliteInit();
+
     i = Global();
 
     i.prefs = await SharedPreferences.getInstance();
     installClientConfig(i.dio);
     i.dio.interceptors.addAll([
+      DioCacheManager(CacheConfig()).interceptor,
       DioThrottler(const Duration(milliseconds: 500)),
     ]);
   }
