2a58a16b4237e9b6877ec7f0fc0acea2548f5aa3
Kazurin Nanako <71819243+Kazurin-775@users.noreply.github.com>
2023-01-31 15:55:49 +0800

    `MessageInputWidget`: Add message send UX
    Currently this is UX-only, so no message will ever be sent.

--- lib/widgets/input.dart
+++ lib/widgets/input.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../creds.dart';
+import '../global.dart';
+import '../messages/multi.dart';
 
 class MessageInputWidget extends StatefulWidget {
   const MessageInputWidget({super.key});
@@ -8,6 +13,11 @@ class MessageInputWidget extends StatefulWidget {
 }
 
 class _MessageInputWidgetState extends State<MessageInputWidget> {
+  final TextEditingController _editController = TextEditingController();
+  final FocusNode _textBoxFocusNode = FocusNode();
+  bool _sendInProgress = false;
+  bool _busy = false;
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -27,25 +37,91 @@ class _MessageInputWidgetState extends State<MessageInputWidget> {
           IconButton(
             onPressed: () {},
             icon: const Icon(Icons.emoji_emotions),
-            color: Colors.blue,
+            // Use gray to indicate busy state (but keep the button enabled).
+            // TODO: better UX design?
+            color: _busy ? Colors.grey : Colors.blue,
           ),
           // Text field
-          const Expanded(
+          Expanded(
             child: TextField(
-              decoration: InputDecoration(
+              controller: _editController,
+              focusNode: _textBoxFocusNode,
+              // Use gray to indicate busy state (but keep the input box enabled).
+              style: TextStyle(color: _busy ? Colors.grey : null),
+              decoration: const InputDecoration(
                 hintText: 'Say something...',
-                focusedBorder: InputBorder.none,
+                border: InputBorder.none,
               ),
+              onSubmitted: (message) {
+                if (message.isNotEmpty) {
+                  _onSend(message);
+                } else {
+                  Global.i.logger.d('Nothing to send');
+                }
+              },
             ),
           ),
           // Send button
           IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.send),
+            // Disable this button when busy
+            // TODO: ternary operator results in ugly formatting. How to resolve this?
+            onPressed: _busy
+                ? null
+                : () {
+                    var message = _editController.text;
+                    if (message.isNotEmpty) {
+                      _onSend(message);
+                    } else {
+                      Global.i.logger.d('Nothing to send');
+                    }
+                  },
+            icon: _busy
+                ? const CircularProgressIndicator()
+                : const Icon(Icons.send),
             color: Colors.blue,
           ),
         ],
       ),
     );
+  }
+
+  void _onSend(String message) async {
+    // Prevent re-entrance (is this ever necessary?)
+    if (_sendInProgress) return;
+    _sendInProgress = true;
+
+    try {
+      setState(() {
+        _busy = true;
+      });
+
+      // Clear input box (to indicate to the user that the app has acknowledged
+      // user input)
+      _editController.text = '';
+      // Re-focus the input box
+      _textBoxFocusNode.requestFocus();
+
+      var roomId =
+          Provider.of<MultiRoomProvider>(context, listen: false).current;
+      var creds = Provider.of<BiliCredsProvider>(context, listen: false);
+
+      if (creds.simulateSend) {
+        // Simulate send
+        Global.i.logger.i('Send message "$message" to room $roomId');
+        await Future.delayed(const Duration(seconds: 1));
+      }
+    } finally {
+      _sendInProgress = false;
+      setState(() {
+        _busy = false;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _editController.dispose();
+    _textBoxFocusNode.dispose();
   }
 }
